8ccf0a20be5d7f75b0ca33cae78a8ab995b5787d
JungLee-Dev <122341199+JungLee-Dev@users.noreply.github.com>
2026-01-22 18:29:54 -0700

    Add additional text prompt to App component

--- App.tsx
+++ App.tsx
@@ -39,6 +39,9 @@ class App extends Component {
         <View>
           <Text>But you can't</Text>
         </View>
+        <View>
+          <Text>Still want to click?</Text>
+        </View>
       </View>
     );
   }
