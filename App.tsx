332264fa20cc1303049cef66e14c7af6271fbf87
JungLee-Dev <122341199+JungLee-Dev@users.noreply.github.com>
2026-01-30 23:29:46 -0700

    Add apology message to App component

--- App.tsx
+++ App.tsx
@@ -51,6 +51,9 @@ class App extends Component {
         <View>
           <Text>Sorry about that</Text>
         </View>
+        <View>
+          <Text>Please understand</Text>
+        </View>
       </View>
     );
   }
