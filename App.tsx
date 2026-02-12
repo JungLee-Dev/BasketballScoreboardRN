0cbe277fd52e91b83a151814b4b2997190a2a2a9
JungLee-Dev <122341199+JungLee-Dev@users.noreply.github.com>
2026-02-11 23:08:53 -0700

    Add additional click prompt to App component

--- App.tsx
+++ App.tsx
@@ -21,6 +21,9 @@ class App extends Component {
         <View>
           <Text>You clicked {this.state.count} times</Text>
         </View>
+        <View>
+          <Text>Click me again</Text>
+        </View>
       </View>
     );
   }
