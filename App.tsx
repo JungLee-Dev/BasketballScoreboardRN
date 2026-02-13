bc84b1809fae06d7c0e39079585e8807ffbf89a6
JungLee-Dev <122341199+JungLee-Dev@users.noreply.github.com>
2026-02-12 22:48:54 -0700

    Remove 'Click me again' text from App component
    Removed redundant text prompting user to click again.

--- App.tsx
+++ App.tsx
@@ -21,9 +21,6 @@ class App extends Component {
         <View>
           <Text>You clicked {this.state.count} times</Text>
         </View>
-        <View>
-          <Text>Click me again</Text>
-        </View>
       </View>
     );
   }
