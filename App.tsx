67a4fa8e52352c5ea5c40969548abe8585cbf75f
JungLee-Dev <122341199+JungLee-Dev@users.noreply.github.com>
2026-02-13 23:58:37 -0700

    Add additional text display in App component

--- App.tsx
+++ App.tsx
@@ -21,6 +21,9 @@ class App extends Component {
         <View>
           <Text>You clicked {this.state.count} times</Text>
         </View>
+        <View>
+          <Text>I know you like it</Text>
+        </View>
       </View>
     );
   }
