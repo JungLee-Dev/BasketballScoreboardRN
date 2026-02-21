8414f84d3963e7ea1df316d4dd71c9c3a4ec7190
JungLee-Dev <122341199+JungLee-Dev@users.noreply.github.com>
2026-02-20 22:46:44 -0700

    Add second clickable button to App component

--- App.tsx
+++ App.tsx
@@ -21,6 +21,9 @@ class App extends Component {
         <View>
           <Text>You clicked {this.state.count} times</Text>
         </View>
+        <TouchableOpacity style={styles.button} onPress={this.onPress}>
+          <Text>Click me2</Text>
+        </TouchableOpacity>
       </View>
     );
   }
