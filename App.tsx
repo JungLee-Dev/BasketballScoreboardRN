4b0bb77b45215b8137f0e692af0cfa027f6adcc7
JungLee-Dev <122341199+JungLee-Dev@users.noreply.github.com>
2026-02-21 22:48:17 -0700

    Add second button to App component

--- App.tsx
+++ App.tsx
@@ -24,6 +24,9 @@ class App extends Component {
         <TouchableOpacity style={styles.button} onPress={this.onPress}>
           <Text>Click me2</Text>
         </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={this.onPress}>
+          <Text>Click me3</Text>
+        </TouchableOpacity>
       </View>
     );
   }
