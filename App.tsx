c943371fe62532c8023d16a26675168cb50498be
JungLee-Dev <122341199+JungLee-Dev@users.noreply.github.com>
2026-02-26 21:56:19 -0700

    Add additional click count display in App.tsx

--- App.tsx
+++ App.tsx
@@ -21,6 +21,9 @@ class App extends Component {
         <View>
           <Text>You clicked {this.state.count} times</Text>
         </View>
+        <View>
+          <Text>You clicked {this.state.count} times 2</Text>
+        </View>
       </View>
     );
   }
